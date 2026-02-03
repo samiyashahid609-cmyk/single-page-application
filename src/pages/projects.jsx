953335ce3bcea8d791b4b54ef794170859f9327a
Samiya Shahid <samiyashahid609@gmail.com>
2026-02-02 23:01:01 -0800

    fix imports and case sensitivity

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Points from '../components/Points'
+
 import Heroes from '../components/shared/hero2'
 
 function Projects() {
   return (
     <div>
       <Heroes />
-      <Points />
+     
     </div>
   )
 }
